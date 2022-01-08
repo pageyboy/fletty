5ee05276f11a00a5b7b241e252e8e7a17f574661
Chris Page <chris_pagey100@hotmail.com>
2022-01-08 17:59:42 +0000

    Improving JSON parsing

--- test.py
+++ test.py
@@ -6,23 +6,34 @@ import secrets
 import requests
 import json
 
-def TimeAndWeather(locationString):
+def GetLatLong(locationString):
     geolocator = Nominatim(user_agent="geoapiExercises")
     print("Location Address:", locationString)
     location = geolocator.geocode(locationString)
     print("Long,Lat:", location.latitude, location.longitude)
+    return [location.latitude, location.longitude]
+
+def GetTime(latitude, longitude):
     obj = TimezoneFinder()
-    result = obj.timezone_at(lng=location.longitude, lat=location.latitude)
+    result = obj.timezone_at(lat=latitude, lng=longitude)
     print("Timezone:",result)
     IST = pytz.timezone(result)
     print("Current Time:", datetime.now(IST))
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&units=metric&appid={secrets.WEATHER_API}"
+
+def GetWeather(latitude, longitude):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={secrets.WEATHER_API}"
     response = requests.get(url)
     data = json.loads(response.text)
-    print(data)
-    print("")
+    for doc in data["weather"]:
+        print(doc['description'])
 
+def TimeAndWeather(locationString):
+    locLatLong = GetLatLong(locationString)
+    locTime = GetTime(locLatLong[0], locLatLong[1])
+    locWeather = GetWeather(locLatLong[0], locLatLong[1])
+    return locTime, locWeather
 
 locations = ["Middlewich, UK", "Colorado Springs, Colorado", "Tokyo, Japan", "Melbourne, Australia", "Cupertino, California", "India"]
 for location in locations:
-    TimeAndWeather(location)
+    locTimeWeather = TimeAndWeather(location)
+    print(locTimeWeather[0], locTimeWeather[1])
