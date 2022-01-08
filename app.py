7ddce62c1469c90b9ac67b96afe2300480d0effa
Chris Page <chris_pagey100@hotmail.com>
2022-01-08 14:40:55 +0000

    Updated output

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to my Flask page!"
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
