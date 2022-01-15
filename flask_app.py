6a31e96cabfa01a8ef0901fe507046ac415055b8
Chris Page <chris_pagey100@hotmail.com>
2022-01-15 09:38:54 +0000

    Updated flask documentation

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,10 @@ from datetime import datetime
 from waitress import serve
 from data_manager import *
 
+"""
+Sets up flask server for returning JSON result of GetData([location list])
+"""
+
 locations = ["Middlewich, UK", "Colorado Springs, Colorado", "Tokyo, Japan", "Melbourne, Australia", "Cupertino, California", "Mumbai, India", "New York City, New York", "Wilmington, Delaware"]
 
 app = Flask(__name__)
